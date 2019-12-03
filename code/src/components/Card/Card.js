939d48dc8016079bcd7efcca64f9ab1d226147c7
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2019-12-03 23:02:00 +0100

    Update like feature

--- code/src/components/Card/Card.js
+++ code/src/components/Card/Card.js
@@ -3,13 +3,16 @@ import styles from './Card.module.css';
 
 const moment = require('moment');
 
-export const Card = ({ data }) => {
+export const Card = ({ data, handleClick }) => {
   return (
     <li>
       <p className={styles.cardMessage}>{data.message}</p>
       <div className={styles.cardInfo}>
         <div className={styles.hearts}>
-          <span className={styles.heartCircle}>
+          <span
+            className={styles.heartCircle}
+            onClick={() => handleClick(data._id)}
+          >
             <i className="fas fa-heart"></i>
           </span>
           <span className={styles.heartCount}>x {data.hearts}</span>
--- code/src/components/Messages/Messages.js
+++ code/src/components/Messages/Messages.js
@@ -3,10 +3,18 @@ import { Card } from '../Card/Card';
 import styles from './Messages.module.css';
 
 export const Messages = ({ data }) => {
+  const handleClick = id => {
+    console.log(id);
+    fetch(`https://technigo-thoughts.herokuapp.com/${id}/like`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  };
+
   return (
     <ul>
       {data.map(item => (
-        <Card key={item._id} data={item} />
+        <Card key={item._id} data={item} handleClick={handleClick} />
       ))}
     </ul>
   );
