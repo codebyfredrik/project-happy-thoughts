ad9af866c194a2012356415e2b28db5bd36204f3
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2019-12-05 20:53:55 +0100

    Update structure, logic and style

--- code/src/components/Card/Card.js
+++ code/src/components/Card/Card.js
@@ -4,15 +4,13 @@ import styles from './Card.module.scss';
 const moment = require('moment');
 
 export const Card = ({ data, handleClick }) => {
+  const { _id: id } = data;
   return (
     <li>
       <p className={styles.cardMessage}>{data.message}</p>
       <div className={styles.cardInfo}>
         <div className={styles.hearts}>
-          <span
-            className={styles.heartCircle}
-            onClick={() => handleClick(data._id)}
-          >
+          <span className={styles.heartCircle} onClick={() => handleClick(id)}>
             <i className="fas fa-heart"></i>
           </span>
           <span className={styles.heartCount}>x {data.hearts}</span>
